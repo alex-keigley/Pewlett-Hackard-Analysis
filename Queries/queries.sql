e2b9d1c5c426c99ac1869dc0746f15c1b2f2f1ca
alex-keigley <30334379+alex-keigley@users.noreply.github.com>
2021-11-17 15:17:22 -0600

    7.3.6 start commit

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -93,4 +93,64 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Creating emp_info table
+SELECT 	e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.gender,
+		s.salary,
+		de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+  	 AND (de.to_date = '9999-01-01');	
+
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+SELECT * FROM manager_info;
+
+-- Department Info
+SELECT 	ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT * FROM dept_info;
+
+
+
+
+
+
+
+
 
